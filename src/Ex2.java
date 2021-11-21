1422e1449c4f11e8a3a1306b5fff58ba991afe55
AmirGill <amirg00@walla.com>
2021-11-21 21:03:02 +0200

    initial commit for Boaz's files

---
+++ src/Ex2.java
@@ -0,0 +1,43 @@
+import api.DirectedWeightedGraph;
+import api.DirectedWeightedGraphAlgorithms;
+
+/**
+ * This class is the main class for Ex2 - your implementation will be tested using this class.
+ */
+public class Ex2 {
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraph getGrapg(String json_file) {
+        DirectedWeightedGraph ans = null;
+        // ****** Add your code here ******
+        //
+        // ********************************
+        return ans;
+    }
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraphAlgorithms getGrapgAlgo(String json_file) {
+        DirectedWeightedGraphAlgorithms ans = null;
+        // ****** Add your code here ******
+        //
+        // ********************************
+        return ans;
+    }
+    /**
+     * This static function will run your GUI using the json fime.
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     *
+     */
+    public static void runGUI(String json_file) {
+        DirectedWeightedGraphAlgorithms alg = getGrapgAlgo(json_file);
+        // ****** Add your code here ******
+        //
+        // ********************************
+    }
+}
