6913ad17beaf0496a0bcf3e947eb1e470a1d8a40
AmirGill <amirg00@walla.com>
2021-11-28 13:06:23 +0200

    Documentations for: DirectedWeightedGraphImpl.java and NodeCompare.java were performed.

--- src/api/DirectedWeightedGraphImpl.java
+++ src/api/DirectedWeightedGraphImpl.java
@@ -5,13 +5,26 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class DirectedWeightedGraphImpl implements DirectedWeightedGraph {
-    /**
-     *
-     */
+
     private HashMap<Integer, NodeData> Vertices;
     private HashMap<String, EdgeData> Edges;
     private HashMap<String, EdgeData> Edges_copy;
 
+    /**
+     *  This constructor gets a JSON_Operation object, to initialize the graph's properties (edges and vertices),
+     *  from the json file which constructed in DirectedWeightedGraphAlgorithmsImpl class.
+     *  The method also initializes a deep copy of the edges for the class' methods.
+     *
+     *
+     *  Brief explanation of our implementation: we have taken a hashmap to support as the collection to have the graph's properties,
+     *  because it is considered the best time complexity for collection's operations with keys (O(1) for the standard operations).
+     *  In order to know where exactly a certain edge from src to dest located in the hashmap,
+     *  we generate the key = "src + dest".
+     *  E.g: given an edge from 0 to 1, then the key for this edge would be the following string "01".
+     *  Additionally, the vertices are ordered with keys by their id.
+     *
+     * @param json - a JSON_Operation object.
+     */
     public DirectedWeightedGraphImpl(JSON_Operation json) {
         json.init_Graph();
         this.Vertices = new HashMap<>();
@@ -25,6 +38,7 @@ public class DirectedWeightedGraphImpl implements DirectedWeightedGraph {
             this.Edges_copy.put("" + edge.getSrc() + edge.getDest(), new EdgeDataImpl(edge));
         }
     }
+
     public DirectedWeightedGraphImpl(HashMap<Integer, NodeData> Vertices,HashMap<String,
             EdgeData> Edges, HashMap<String, EdgeData> Edges_copy) {
        this.Edges_copy = Edges_copy;
@@ -63,11 +77,14 @@ public class DirectedWeightedGraphImpl implements DirectedWeightedGraph {
     }
 
     /**
-     * NOTICE: IF YOU CHANGE THE VALUES OF THE HASHMAP COPY'S OBJECTS, IT CHANGES ALSO THE VALUES OF THE REAL HASHMAP.
-     * EXAMINE DEEP COPY FOR EACH OBJECT IN THE HASHMAP
+     * As the method "removeNode", this method removes, allegedly, the vertex.
+     * It is actually removes all edges starts or ends with the given node.
+     * However, instead of removing it from the graph, because it isn't what explicitly written in the interface,
+     * it removes it from a copy already constructed in the constructor.
+     * Then, returns the iterator for this copy edges collection.
      *
-     * @param node_id
-     * @return
+     * @param node_id - a given node's id.
+     * @return an iterator of the edges after removing all edges start or end in this given vertex.
      */
     @Override
     public Iterator<EdgeData> edgeIter(int node_id) {
@@ -76,21 +93,39 @@ public class DirectedWeightedGraphImpl implements DirectedWeightedGraph {
             if (node_id == i) {continue;}
             String currKey = "" + node_id + i;
             String currKey2 = "" + i + node_id;
-            Edges.remove(currKey);
-            Edges.remove(currKey2);
+            Edges_copy.remove(currKey);
+            Edges_copy.remove(currKey2);
 
         }
-        return Edges.values().iterator();
+        return Edges_copy.values().iterator();
     }
-
+    /**
+     *
+     * The method goes over all vertices and checks which vertex has an edge with the given node.
+     * If it finds such vertex it removes the edge between them.
+     * Finally, the method removes the vertex from the vertices' collection (Hashmap).
+     *
+     * In terms of time complexity, we here got O(V) rather than O(k).
+     * The reason for that is that our implementation works the way in which an edge from src to dest,
+     * could be found in the hash map of the edges under the key = "src + dest".
+     * E.g: given an edge from 0 to 1, then the key for this edge would be the following string "01".
+     * Therefore, we have to go through all strings which starts with the given key, or ends there,
+     * and that's the reason why we need to go over all vertices.
+     * The different between O(V) and O(k) is a bit of a pain, but with a further look
+     * it makes no different if the graph is complete, or if it's a standard graph with
+     * high amount of edges.
+     *
+     * @param key - a given id of a node.
+     * @return the removed node.
+     */
     @Override
     public NodeData removeNode(int key) {
         for (int i = 0; i < Vertices.size(); i++) {
             if (key == i) {continue;}
             String currKey = "" + key + i;
             String currKey2 = "" + i + key;
-            Edges_copy.remove(currKey);
-            Edges_copy.remove(currKey2);
+            Edges.remove(currKey);
+            Edges.remove(currKey2);
         }
         return Vertices.remove(key);
     }
@@ -131,9 +166,9 @@ public class DirectedWeightedGraphImpl implements DirectedWeightedGraph {
         Edges = edges;
     }
     /**
-     * performs a deep copy of a graph g
-     * @param g
-     * @return
+     * This method gets a graph - g and performs a deep copy.
+     * @param g - a given directed weighted graph to (deep) copy.
+     * @return the (deep) copy of the graph - g.
      */
     public DirectedWeightedGraph deepCopy(DirectedWeightedGraphImpl g) {
         HashMap<Integer, NodeData> Vertices = new HashMap<>();
--- src/api/NodeCompare.java
+++ src/api/NodeCompare.java
@@ -2,21 +2,23 @@ package api;
 
 
 class NodeCompare implements Comparable<NodeCompare> {
+
     private NodeData vertex;
 
-    public NodeCompare(NodeData v) {
-
-        this.vertex = v;
-
-    }
+    public NodeCompare(NodeData v) {this.vertex = v;}
 
     public NodeData getVertex() {
         return vertex;
     }
-
+    /**
+     * This function helps to compare between two nodes, by their weights.
+     * The lower, the better.
+     * @param o - an object to compare with.
+     * @return 1 if the curr vertex has lower weight than the given node.
+     *         Otherwise, it returns -1.
+     */
     @Override
     public int compareTo(NodeCompare o) {
         return Double.compare(this.vertex.getWeight(), o.getVertex().getWeight());
-
     }
 }
