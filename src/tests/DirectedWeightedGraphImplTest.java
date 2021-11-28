3300a71b002567651e316349daa9f4523cfb3d79
AmirGill <amirg00@walla.com>
2021-11-28 20:56:08 +0200

    Test DirectedWeightedGraphImplTest has fixed.

--- src/tests/DirectedWeightedGraphImplTest.java
+++ src/tests/DirectedWeightedGraphImplTest.java
@@ -4,18 +4,20 @@ import api.*;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DirectedWeightedGraphImplTest {
+
     private HashMap<Integer, NodeData> Vertices;
     private HashMap<String, EdgeData> Edges;
     private DirectedWeightedGraph randGraph;
 
-    @Before
-    public void init() {
-        int randCapacityVerts = 1 + (int) (Math.random() * 30);
+    public DirectedWeightedGraphImplTest() {
+        int randCapacityVerts = 3 + (int) (Math.random() * 30);
         this.Vertices = new HashMap<>(randCapacityVerts);
         int randCapacityEdges = 1 + (int) (Math.random() * (randCapacityVerts * (randCapacityVerts - 1)));
         this.Edges = new HashMap<>(randCapacityEdges);
@@ -31,6 +33,8 @@ class DirectedWeightedGraphImplTest {
         // Initializes the random graph
         randGraph = new DirectedWeightedGraphImpl(Vertices, Edges);
     }
+
+
     /**
      * This method creates a random node, when random node defined to have random values.
      *
@@ -39,7 +43,7 @@ class DirectedWeightedGraphImplTest {
     public NodeData random_vertex_generator(int key) {
         int tag = 1 + (int) (Math.random() * 10);
         double weight = Math.random() * 10;
-        double x = Math.random() * 40, y = Math.random() * 40, z = Math.random() * 40;
+        double x = Math.random() * 10, y = Math.random() * 10, z = Math.random() * 10;
         GeoLocation Node = new GeoLocationImpl(x, y, z);
         String info = "Key:" + key + "\n" + "Tag:" + tag + "\n" + "Weight" + weight + "\n" + "GeoLocation:" + Node;
         return new NodeDataImpl(key, tag, info, weight, Node);
@@ -52,95 +56,96 @@ class DirectedWeightedGraphImplTest {
             dest = (int) (Math.random() * this.Vertices.size());
         }
         int tag = 1 + (int) (Math.random() * 10);
-        double weight = Math.random() * 20;
+        double weight = Math.random() * 10;
         String info = "Src:" + src + "\n" + "Dest:" + dest + "\n" + "Tag" + tag + "\n" + "Weight:" + weight;
         return new EdgeDataImpl(src, dest, tag, weight, info);
     }
 
+    public int[] two_random_distinct_numbers(int a, int b) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = a; i < b; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        return new int[]{numbers.get(0), numbers.get(1)};
+    }
+
+
     @Test
     void getNode() {
-        init();
-        int randKey = (int) (Math.random() * randGraph.nodeSize());
-        System.out.println(randKey);
+        int randKey = (int) (Math.random() * (randGraph.nodeSize() - 1));
         String info = randGraph.getNode(randKey).getInfo();
         assertEquals(randGraph.getNode(randKey).getInfo(), info);
     }
 
     @Test
     void getEdge() {
-        init();
-        int src = (int) (Math.random() * randGraph.nodeSize());
-        int dest = (int) (Math.random() * randGraph.nodeSize());
-        System.out.println(src + "," + dest);
-        while (randGraph.getEdge(src, dest) == null) {
-            dest = (int) (Math.random() * randGraph.nodeSize());
-        }
+        int[] srcDest = two_random_distinct_numbers(0, randGraph.nodeSize() - 1);
+        int src = srcDest[0];
+        int dest = srcDest[1];
+        randGraph.connect(src, dest, Math.random() * 10);
         String info = randGraph.getEdge(src, dest).getInfo();
         assertEquals(randGraph.getEdge(src, dest).getInfo(), info);
     }
 
     @Test
     void addNode() {
-        init();
         int size = randGraph.nodeSize();
         NodeData add = random_vertex_generator(randGraph.nodeSize());
         randGraph.addNode(add);
-        assertEquals(size+1,randGraph.nodeSize());
+        assertEquals(size + 1, randGraph.nodeSize());
     }
 
     @Test
     void connect() {
-/*        init();
-        int src = (int) (Math.random() * randGraph.nodeSize());
-        int dest = (int) (Math.random() * randGraph.nodeSize());
-
-        while (randGraph.getEdge(src, dest) == null) {
-            dest = (int) (Math.random() * randGraph.nodeSize());
-        }
-        double weight = randGraph.getEdge(src,dest).getWeight();
-        //assertNotEquals(randGraph.connect(src,dest,weight),randGraph.connect(src,dest,weight));*/
+        int size = randGraph.edgeSize();
+        EdgeData edge = random_edge_generator();
+        if (randGraph.getEdge(edge.getSrc(), edge.getDest()) == null) {
+            randGraph.connect(edge.getSrc(), edge.getDest(), edge.getWeight());
+            assertEquals(size + 1, randGraph.edgeSize());
+        } else
+            assertEquals(size, randGraph.edgeSize());
     }
+
     @Test
     void removeNode() {
-        init();
         int size = randGraph.nodeSize();
-        int randKey = (int) (Math.random() * randGraph.nodeSize());
+        int randKey = (int) (Math.random() * (randGraph.nodeSize()-1));
         randGraph.removeNode(randKey);
-        assertEquals(size-1,randGraph.nodeSize());
+        assertEquals(size - 1, randGraph.nodeSize());
         assertNull(randGraph.getNode(randKey));
     }
 
     @Test
     void removeEdge() {
-        init();
         int size = randGraph.edgeSize();
-        int src = (int) (Math.random() * randGraph.nodeSize());
-        int dest = (int) (Math.random() * randGraph.nodeSize());
-        while (randGraph.getEdge(src, dest) == null) {
-            dest = (int) (Math.random() * randGraph.nodeSize());
+        int[] srcDest = two_random_distinct_numbers(0, randGraph.nodeSize() - 1);
+        int src = srcDest[0];
+        int dest = srcDest[1];
+        if (randGraph.getEdge(src, dest) == null) {
+            assertEquals(size, randGraph.edgeSize());
         }
-        randGraph.removeEdge(src,dest);
-        assertEquals(size-1,randGraph.edgeSize());
-        assertNull(randGraph.getEdge(src,dest));
-    }
+        else{
+            randGraph.removeEdge(src, dest);
+            assertEquals(size - 1, randGraph.edgeSize());
+            assertNull(randGraph.getEdge(src, dest));
+        }
+}
 
     @Test
     void nodeSize() {
-        init();
         int size = randGraph.nodeSize();
         assertEquals(randGraph.nodeSize(),size);
     }
 
     @Test
     void edgeSize() {
-        init();
         int size = randGraph.edgeSize();
         assertEquals(randGraph.edgeSize(),size);
     }
 
     @Test
     void getMC() {
-        init();
         int count = randGraph.getMC();
         assertEquals(randGraph.getMC(),count);
     }
