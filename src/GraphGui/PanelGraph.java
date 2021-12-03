da7b697429a84e99b7b8be1d392cefc9a022451c
AmirGill <amirg00@walla.com>
2021-12-03 03:11:46 +0200

    Changes by Gal:
    Panel calculation functions added, for the graph design.

--- src/GraphGui/PanelGraph.java
+++ src/GraphGui/PanelGraph.java
@@ -43,32 +43,63 @@ public class PanelGraph extends JPanel {
         super.paintComponent(g);
 
         Graphics2D g2d = (Graphics2D) g.create();
-
         FontMetrics fm = g2d.getFontMetrics();
         double insets = fm.getHeight() + radius;
+
+        for (GraphEdge ed : edges) {
+            ArrayList<GraphPoint> p = ed.getPoints();
+            paintLine(g2d, p.get(0), p.get(1), insets, ed.getWeight());
+        }
 
         for (GraphPoint gp : points) {
             paintPoint(g2d, gp, insets);
         }
-
-        for (GraphEdge ed : edges) {
-            ArrayList<GraphPoint> p = ed.getPoints();
-            paintLine(g2d,p.get(0),p.get(1),insets,ed.getWeight());
-        }
-
         g2d.dispose();
     }
 
+    private boolean CheckEdge(GraphPoint from, GraphPoint to){
+        return graph.getEdge(Integer.parseInt(from.getId()), Integer.parseInt(to.getId())) != null;
+    }
+
+    protected double angleBetween(Point2D from, Point2D to) {
+        double x = from.getX();
+        double y = from.getY();
+        double deltaX = to.getX() - x;
+        double deltaY = to.getY() - y;
+        double rotation = -Math.atan2(deltaX, deltaY);
+        rotation = Math.toRadians(Math.toDegrees(rotation) + 180);
+        return rotation;
+    }
+
+    protected Point2D getPointOnCircle(Point2D center, double radians) {
+        double x = center.getX();
+        double y = center.getY();
+        radians = radians - Math.toRadians(90.0);
+        double xPosy = Math.round((float) (x + Math.cos(radians) * radius));
+        double yPosy = Math.round((float) (y + Math.sin(radians) * radius));
+        return new Point2D.Double(xPosy, yPosy);
+    }
+
     private void paintLine(Graphics2D g2d, GraphPoint from, GraphPoint to, double insets, String weight) {
+        boolean flag = CheckEdge(to,from);
         Point2D fromPoint = translate(from, insets);
         Point2D toPoint = translate(to, insets);
         g2d.setColor(Color.RED);
-        g2d.draw(new Line2D.Double(fromPoint, toPoint));
-        drawArrowHead(g2d, toPoint, fromPoint, Color.WHITE);
-        StringWeight(g2d, weight, to, from, insets);
+        //g2d.draw(new Line2D.Double(fromPoint, toPoint));
+        // drawArrowHead(g2d, toPoint, fromPoint, Color.WHITE);
+        //StringWeight(g2d, weight, to, from, insets);
+        //================================================
+        double fromp = angleBetween(fromPoint, toPoint);
+        double top = angleBetween(toPoint, fromPoint);
+        Point2D pointFrom = getPointOnCircle(fromPoint, fromp);
+        Point2D pointTo = getPointOnCircle(toPoint, top);
+        Line2D line = new Line2D.Double(pointFrom, pointTo);
+        drawArrowHead(g2d, pointTo, pointFrom, Color.RED);
+        g2d.draw(line);
+        StringWeight(g2d, weight, to, from, insets, flag);
     }
 
-    private void StringWeight(Graphics2D g2d, String weight, GraphPoint to, GraphPoint from, double insets) {
+    private void StringWeight(Graphics2D g2d, String weight, GraphPoint to, GraphPoint from, double insets,boolean check) {
         Point2D translated = translate(from, insets);
         Point2D translated2 = translate(to, insets);
 
@@ -85,7 +116,23 @@ public class PanelGraph extends JPanel {
         double y_center = (yPos + (yPos2)) / 2;
         g2d.setPaint(Color.black);
         if (weight.length() > 13) weight = weight.substring(0, weight.length() - 12);
-        //g2d.drawString(weight, (float) x_center, (float) y_center);
+        g2d.setPaint(Color.black);
+        g2d.setFont(new Font("Cabin", Font.PLAIN, 13));
+        g2d.drawString(weight, (float) x_center, (float) y_center);
+    }
+
+    private void Verticle(double x1, double y1, double m_Segment, int length){
+        double m = -1/m_Segment;
+        // y = mx - mx1 + y1
+        double k = (1/Math.pow(m,2)) + 1;
+        double c = -2*(((x1/m) + y1)) + Math.pow(y1,2) + Math.pow(x1,2) - Math.pow(length,2);
+
+        double x1_ans = x1 + ((Math.sqrt((x1*x1*k*k)-(k*c)))/k);
+        double x2_ans = -x1_ans;
+
+        // finds the y for the resulted points.
+        double y1_ans = m*x1_ans - m*x1 + y1;
+        double y2_ans = m*x2_ans - m*x1 + y1;
     }
 
     private void paintPoint(Graphics2D g2d, GraphPoint gp, double insets) {
@@ -107,10 +154,12 @@ public class PanelGraph extends JPanel {
         double x = xPos - (fm.stringWidth(text) / 2);
         double y = (yPos - radius - fm.getHeight()) + fm.getAscent();
         g2d.setPaint(Color.black);
-        g2d.drawString(text, (float)x, (float)y);
+        g2d.setFont(new Font("Cabin", Font.BOLD, 14));
+        g2d.drawString(text, (float) x, (float) y);
 
         g2.dispose();
     }
+
     private void drawArrowHead(Graphics2D g2, Point2D tip, Point2D tail, Color color) {
         int ArrowSize = 10;
         g2.setPaint(color);
@@ -146,6 +195,7 @@ public class PanelGraph extends JPanel {
 
         return new Point2D.Double(x, y);
     }
+
     /*
      * the for loop is init all the edges existed in the graph and take each point already exists,
      * make a new arraylist of points and know to the correct edges between them.
@@ -158,18 +208,19 @@ public class PanelGraph extends JPanel {
                 if (i == j) continue;
                 ArrayList<GraphPoint> temp = new ArrayList<>();
                 if (graph.getEdge(i, j) != null) {
-                    temp.add(new GraphPoint(String.valueOf(graph.getNode(graph.getEdge(i,j).getSrc()).getKey()),new Point2D.Double(graph.getNode(graph.getEdge(i, j).getSrc()).getLocation().x(), graph.getNode(graph.getEdge(i, j).getSrc()).getLocation().y())));
-                    temp.add(new GraphPoint(String.valueOf(graph.getNode(graph.getEdge(i,j).getDest()).getKey()),new Point2D.Double(graph.getNode(graph.getEdge(i, j).getDest()).getLocation().x(), graph.getNode(graph.getEdge(i, j).getDest()).getLocation().y())));
+                    temp.add(new GraphPoint(String.valueOf(graph.getNode(graph.getEdge(i, j).getSrc()).getKey()), new Point2D.Double(graph.getNode(graph.getEdge(i, j).getSrc()).getLocation().x(), graph.getNode(graph.getEdge(i, j).getSrc()).getLocation().y())));
+                    temp.add(new GraphPoint(String.valueOf(graph.getNode(graph.getEdge(i, j).getDest()).getKey()), new Point2D.Double(graph.getNode(graph.getEdge(i, j).getDest()).getLocation().x(), graph.getNode(graph.getEdge(i, j).getDest()).getLocation().y())));
                     edges.add(new GraphEdge(String.valueOf(graph.getEdge(i, j).getWeight()), temp));
                 }
             }
         }
     }
+
     /*
      * the for loop is init all the points in the graph.
      * By given a name of the vertex and the positions x,y.
      * */
-    public void pointInit(){
+    public void pointInit() {
         for (int i = 0; i < graph.nodeSize(); i++) {
             NodeData curr = graph.getNode(i);
             if (curr != null) {
@@ -180,11 +231,12 @@ public class PanelGraph extends JPanel {
             }
         }
     }
+
     /*
      * search if the point is equal to the string id  and get the point from the points objects.
      * draw each line have edges.
      */
-    public void setMinMaxRange(){
+    public void setMinMaxRange() {
 
         double minX = Double.MAX_VALUE;
         double maxX = Double.MIN_VALUE;
